d1787fc4bc7ec5fa801df6b2ce561dfc9f3bcff5
Mariusz Kuberczyk <32803007+queros@users.noreply.github.com>
2019-12-16 16:12:14 +0100

    Brand new tokens management (#9)
    * remove reportedby property + get userid from token
    
    * modify token management with refresh token
    
    * modify signin method
    
    * add missing line break

--- server/src/modules/reports/reports.resolver.ts
+++ server/src/modules/reports/reports.resolver.ts
@@ -1,11 +1,12 @@
 import { Inject, UseGuards } from '@nestjs/common';
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { AuthGuard } from '../auth/guards/user-auth.guard';
 import { ObjectIdScalar } from '../common/graphql-scalars/object-id.scalar';
 import { AddReportInput } from './models/add-report.input';
 import { EditReportInput } from './models/edit-report.input';
 import { Report } from './models/report.schema';
 import { ReportsService } from './reports.service';
+import { ICurrentUser } from '../auth/interfaces/current-user.interface';
 
 @Resolver(() => Report)
 export class ReportsResolver {
@@ -27,14 +28,16 @@ export class ReportsResolver {
   @UseGuards(AuthGuard)
   async addReport(
     @Args('report') reportInput: AddReportInput,
+    @Context('user') user: ICurrentUser,
   ): Promise<Report> {
-    return this.reportsService.add(reportInput);
+    return this.reportsService.add(reportInput, user);
   }
 
   @Mutation(() => Report)
   async editReport(
     @Args('report') reportInput: EditReportInput,
+    @Context('user') user: ICurrentUser,
   ): Promise<Report> {
-    return this.reportsService.edit(reportInput);
+    return this.reportsService.edit(reportInput, user);
   }
 }
