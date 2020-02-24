ec929b840480ef7b357e4e5b334e84b59c12af7c
Oskar Morawiec <oskar.morawiec@gorrion.io>
2020-02-24 12:18:25 +0100

    Added isActivated flag to payload. Added eslint rule to disable ract props warning

--- server/src/modules/auth/auth.service.ts
+++ server/src/modules/auth/auth.service.ts
@@ -179,6 +179,7 @@ export class AuthService {
       _id: user._id,
       userName: user.userName,
       role: user.role,
+      isActivated: user.isActivated ? true : false,
     };
 
     const tokenResponse = new Token();
